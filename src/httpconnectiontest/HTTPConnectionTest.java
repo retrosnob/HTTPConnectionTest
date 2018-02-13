3aefbc652b52d5f9b88b13fbb400d6c21b66b945
retrosnob <jusrobertson@yahoo.co.uk@>
2018-02-13 15:49:04 +0800

    Whole project first commit.

---
+++ src/httpconnectiontest/HTTPConnectionTest.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package httpconnectiontest;
+
+import java.net.*;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+/**
+ *
+ * @author justin
+ */
+public class HTTPConnectionTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        try {
+        URL url = new URL("http://127.0.0.1:8081/java");
+        HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+        httpURLConnection.setRequestMethod("POST");
+        httpURLConnection.setRequestProperty("Content-Type", "application/json;charset=UTF-8");
+        httpURLConnection.setRequestProperty("Accept", "application/json");
+        httpURLConnection.setDoOutput(true);
+        httpURLConnection.setDoInput(true);
+        InputStream inputStream = httpURLConnection.getInputStream();
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
